4688322f6746631a4569df641e4cfa87463fc0ea
Rajaryan99 <raj.aryan2518@gmail.com~>
2026-06-06 23:33:30 +0530

    used a grid layout to format blog posts

--- next-basic/src/app/blogs/page.js
+++ next-basic/src/app/blogs/page.js
@@ -1,27 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await data.json()
 
 
-  const posts = [
-    {
-      id: 'one',
-      title: "First Post",
-      content: "This is the content of the first post."
-    },
-    {
-    id: 2,
-    title: "Second Post",
-    content: "This is the content of the second post."
-  },
-   {
-    id: 3,
-    title: "Third Post",
-    content: "This is the content of the third post."
-  }
+  // const posts = [
+  //   {
+  //     id: 'one',
+  //     title: "First Post",
+  //     content: "This is the content of the first post."
+  //   },
+  //   {
+  //   id: 2,
+  //   title: "Second Post",
+  //   content: "This is the content of the second post."
+  // },
+  //  {
+  //   id: 3,
+  //   title: "Third Post",
+  //   content: "This is the content of the third post."
+  // }
 
-  ]
+  // ]
 
   console.log('posts = ' + posts)
 
@@ -32,14 +35,19 @@ export default function page() {
       <h1 className=' '>Blogs</h1>
       </div>
 
+      <div className='grid grid-cols-3 gap-4 p-4'>
+
       {posts.map((post) => (
-        <div key={post.id}>
+        <div key={post.id} className='p-4 m-4 h-[400px] w-[350px]  overflow-hidden border-2 border-gray-300 rounded-lg '>
           <Link href={`/blogs/${post.id}`}>
-            <h2 className='font-bold text-blue-400 text-3xl'>{post.title}</h2>
+            <h2 className='font-bold text-xl underline text-amber-600 transition'>{post.title}</h2>
           </Link>
-          <p>{post.content}</p>
+          <p>{post.body}</p>
         </div>
+
+
       ))}
+      </div>
     </>
   )
 }
