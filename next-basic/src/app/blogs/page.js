3c7f8f17e35e44e3c11763d48c32c9e936cc8798
Rajaryan99 <raj.aryan2518@gmail.com~>
2026-06-07 12:04:25 +0530

    added no store catch to get fresh data

--- next-basic/src/app/blogs/page.js
+++ next-basic/src/app/blogs/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default async function page() {
 
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts', {cache: "no-store"})
   const posts = await data.json()
 
 
